6615be304f275820bac7fc141340def34bf202c2
takahahiro0224 <hikusema189@keio.jp>
2020-01-14 09:48:22 +0900

    簡易ログイン画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
-  protect_from_forgery with: :null_session
+  #include DeviseTokenAuth::Concerns::SetUserByToken
+  #skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
+  #protect_from_forgery with: :null_session
 
 
 end
