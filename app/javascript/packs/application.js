838e4f6f4a25bb042c061af58eb48ee7ac461dec
kyohei <nacsan654@gmail.com>
2020-01-31 01:51:46 +0900

    ログイン画面のデザイン適用

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require('jquery')
 
 import '../stylesheets/application.scss'
 import '../stylesheets/registrations.scss'
+import '../stylesheets/sessions.scss'
 import 'materialize-css/dist/js/materialize.min.js'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
