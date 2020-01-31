24ead0c4f77b03b5f856eaf55fb5c3f12fddb635
takahahiro0224 <hikusema189@keio.jp>
2020-01-31 18:54:47 +0900

    環境変数設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module DigitalSignApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.hosts << "moneycorrect.herokuapp.com"
     
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
